a28be7882abaf3820ffd371ef3d3b51143ee8245
Nicholas <nthompson@slb.com>
2015-09-15 10:09:01 -0500

    More sensibile arrangement of ideas

--- v19/writer.py
+++ v19/writer.py
@@ -3,8 +3,10 @@
 import h5py
 import numpy as np
 import time
+import os
 from multiprocessing import Process, Event
 
+os.remove('growing.h5')
 f = h5py.File('growing.h5', 'w', libver='latest')
 arr = np.array([1,2,3,4])
 
@@ -16,7 +18,7 @@ while True:
     new_shape = (2*len(arr), )
     arr = np.ones(shape=new_shape)
     dset.resize(new_shape)
-    print("Writing data:")
+    print("Writing data of length {}".format(len(arr)))
     dset[...] = arr
     dset.flush()
     time.sleep(5)
